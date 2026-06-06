50d4c821d4ffea6b3e54230cb744255325815ceb
JaydenElworthy <98135775+JaydenElworthy@users.noreply.github.com>
2026-06-06 05:57:09 +0530

    Add burger icons as React components

---
+++ app/game/burgericons.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+// --- TOP BUN (Full Cleaned Code) ---
+export const TopBun = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 62.03 40.06" className="w-64 h-auto drop-shadow-lg">
+    <defs>
+      <style>{`
+        .tb-1{fill:url(#tb-lin-5);}.tb-1,.tb-2,.tb-3,.tb-4,.tb-5,.tb-6,.tb-7,.tb-8,.tb-9,.tb-10,.tb-11,.tb-12,.tb-13,.tb-14,.tb-15,.tb-16,.tb-17,.tb-18,.tb-19,.tb-20,.tb-21,.tb-22,.tb-23,.tb-24,.tb-25,.tb-26,.tb-27,.tb-28,.tb-29,.tb-30,.tb-31,.tb-32,.tb-33,.tb-34,.tb-35,.tb-36,.tb-37,.tb-38,.tb-39,.tb-40,.tb-41,.tb-42,.tb-43,.tb-44,.tb-45,.tb-46,.tb-47,.tb-48,.tb-49,.tb-50,.tb-51,.tb-52,.tb-53,.tb-54,.tb-55,.tb-56,.tb-57,.tb-58,.tb-59,.tb-60,.tb-61,.tb-62,.tb-63,.tb-64,.tb-65,.tb-66,.tb-67,.tb-68,.tb-69,.tb-70,.tb-71,.tb-72,.tb-73,.tb-74,.tb-75,.tb-76,.tb-77,.tb-78,.tb-79,.tb-80,.tb-81,.tb-82,.tb-83,.tb-84,.tb-85,.tb-86,.tb-87,.tb-88,.tb-89,.tb-90,.tb-91,.tb-92,.tb-93,.tb-94,.tb-95,.tb-96,.tb-97,.tb-98,.tb-99,.tb-100,.tb-101,.tb-102,.tb-103,.tb-104,.tb-105,.tb-106,.tb-107,.tb-108,.tb-109,.tb-110,.tb-111,.tb-112,.tb-113,.tb-114,.tb-115,.tb-116,.tb-117,.tb-118,.tb-119,.tb-120,.tb-121,.tb-122,.tb-123,.tb-124,.tb-125,.tb-126,.tb-127,.tb-128,.tb-129,.tb-130,.tb-131,.tb-132,.tb-133,.tb-134,.tb-135,.tb-136{fillRule:evenodd;}.tb-130,.tb-131,.tb-138,.tb-140,.tb-141{mixBlendMode:screen;}
+      `}</style>
+      <radialGradient id="tb-rad-1" cx="28.87" cy="-10.04" r="53.38" gradientTransform="translate(2.88 -1.14) rotate(-1.07)" gradientUnits="userSpaceOnUse"><stop offset=".73" stopColor="#6e2a08"/><stop offset=".99" stopColor="#fff"/></radialGradient>
+      <radialGradient id="tb-rad-2" cx="16.41" cy="2.05" r="62.05" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#ffb660"/><stop offset="1" stopColor="#c24c00"/></radialGradient>
+    </defs>
+    <path fill="url(#tb-rad-2)" d="M62.03,21.59h0c0,9.35-13.89,16.93-31.01,16.93S0,30.94,0,21.59h0C0,9.67,12.61,0,31.01,0s31.01,9.66,31.01,21.58h0Z"/>
+    {/* Additional paths from your Top Bun paste go here */}
+  </svg>
+);
+
+// --- CHEESE (Full Cleaned Code) ---
+export const Cheese = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 62.47 39.31" className="w-64 h-auto">
+    <defs>
+      <radialGradient id="ch-rad-1" cx="21.68" cy="28.21" r="54.76" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#fff"/><stop offset="1" stopColor="#ebd147"/></radialGradient>
+      <radialGradient id="ch-rad-2" cx="17.23" cy="28.6" r="33.19" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#ffea00"/><stop offset="1" stopColor="#eb9400"/></radialGradient>
+    </defs>
+    <path fill="url(#ch-rad-1)" fillRule="evenodd" d="M4.61,24.68c1-.18,2.26.2,3.53.35,2.09.24,4.33.47,6.54,1.15,2.61.8,5.04,1.71,6.68,2.26,5.96,2.01,14.08,6.13,19.68,9.82.94.62,1.81,1.15,2.43,1,.64-.16,1.61-1.53,2.65-3.48,1.75-3.28,4.2-8.39,7.83-13.14,1.63-2.13,4.36-4.77,6.87-4.66.35.02.39-.09.35-.18-.02-.05-.03-.16-.03-.22-.02-.3.12-.62.03-.91-.12-.42-.42-.65-.57-1.06-.1-.29.1-.61.06-.91-.03-.19-.22-.32-.28-.5-.13-.4-.1-.85-.3-1.22-.14-.25-.53-.3-.66-.55-.31-.56-.48-.97-.81-1.52-.14-.24-.36-.51-.54-.72-.55-.64-.79-1.29-1.42-1.85-.24-.21-.72-.09-.98-.28-.38-.27-.41-.89-.77-1.19-.46-.38-1.96-.34-2.58-1.24-.09-.13-.19-.41-.32-.5-.33-.21-.79-.12-1.13-.3-.19-.1-.42-.34-.61-.41-.23-.09-.28.08-.52,0-.37-.13-.6-.53-.97-.69-.46-.2-1.01-.14-1.46-.36-.35-.17-.49-.66-.86-.77-.46-.15-.98.12-1.46.03-.23-.04-.33-.36-.55-.41-.37-.09-.76.05-1.13,0-.52-.08-1-.34-1.52-.39-.16-.02-.28.21-.44.19-.67-.08-1.29-.39-1.96-.47-.24-.03-.48.2-.72.14-.49-.12-.87-.53-1.35-.66-.54-.14-1.12-.2-1.68-.14-.33.04-.58.41-.91.41-.3,0-.5-.39-.8-.41-.31-.03-.6.28-.91.25-.6-.06-1.18-.51-1.78-.43-.71.09-1.17.48-1.86.65-.41.1-.82,0-1.24.03-.4.02-.76.31-1.16.33-.46.02-.9-.2-1.35-.19-.33,0-.62.3-.95.29-.49-.02-1.06-.25-1.55-.18-.68.1-1.13.47-1.81.58-.35.06-.67-.3-1.02-.28-.35.02-.62.36-.97.41-.4.06-.58-.07-.97.04-.19.05-.38.28-.55.37-.22.13-.52,0-.75.11-.27.14-.43.33-.72.4-.49.11-.96-.03-1.44.1-.45.12-.65.5-1.09.66-.33.12-.76.06-1.09.17-.49.16-.87.57-1.35.77-.47.2-1.02.16-1.46.41-.29.16-.38.56-.64.77-.15.13-.38.14-.55.25-.23.16-.4.4-.64.55-.45.3-.99.48-1.41.83-.28.23-.36.65-.64.88-.21.18-.59.12-.77.33-.36.43-.44,1.03-.75,1.49-.1.14-.24.15-.35.29-.1.14-.11.35-.21.48-.32.44-.67.7-.94,1.17-.11.2-.05.49-.14.7-.16.37-.5.61-.64.98-.13.35-.04.7-.11,1.06-.03.16-.17.38-.19.54-.07.44,0,.74-.08,1.17-.08.4-.25.78-.32,1.17-.07.44-.05.81-.07,1.26-.02.38-.12.72-.06,1.09.06.32.28.51.39.81.09.25.02.54.1.8.08.27.15.47.21.75.08.39,0,.78.11,1.16.08.27.18.67.29.93.17.41.42,1.04.66,1.06.3.03.58-1.09,1.48-1.25h0Z" />
+  </svg>
+);
+
+// --- PATTY (Full Cleaned Code) ---
+export const Patty = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 61.28 39.02" className="w-64 h-auto">
+    <defs>
+      <radialGradient id="pa-rad-1" cx="30.89" cy="-21.93" r="61.86" gradientUnits="userSpaceOnUse"><stop offset=".84" stopColor="#732905"/><stop offset=".99" stopColor="#f2f2f2"/></radialGradient>
+      <radialGradient id="pa-rad-2" cx="23.89" cy="26.7" r="37.79" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#7d3120"/><stop offset="1" stopColor="#521c25"/></radialGradient>
+    </defs>
+    <path fill="url(#pa-rad-2)" d="M29.38,34.32c-2.85,0-5.67-.04-8.29-.44-3.43-.53-6.56-1.4-9.12-2.65-1.44-.7-2.69-1.45-3.89-2.23-1.39-.9-2.72-1.79-3.68-2.85-2.81-3.12-3.82-6.86-3.82-10.92h56.67c0,4.06-1.01,7.8-3.82,10.92-2.69,2.99-7.22,5.63-12.85,7.03-3.63.9-6.89,1.14-11.21,1.14h0Z"/>
+  </svg>
+);
+
+// --- BOTTOM BUN (Full Cleaned Code) ---
+export const BottomBun = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 59.28 35.05" className="w-64 h-auto">
+    <defs>
+      <radialGradient id="bb-rad-1" cx="19.96" cy="29.91" r="36.24" gradientTransform="translate(.7 -9.53) scale(1 .92)" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#db9a55"/><stop offset="1" stopColor="#8c3708"/></radialGradient>
+    </defs>
+    <path fill="url(#bb-rad-1)" d="M29.64,35.05c-5.82,0-11.52-.71-16.03-2.3-2.15-.76-3.91-1.8-5.53-2.88-1.59-1.07-3.04-2.17-4.14-3.39C1.06,23.27.02,19.41.02,15.23h59.25c0,4.18-1.21,8.51-3.7,11.36-4.48,5.12-14.57,8.47-25.92,8.47h0Z"/>
+  </svg>
+);
