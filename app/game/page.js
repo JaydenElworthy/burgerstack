af2c443e24b917bb40e753bb880351701b01a5d1
JaydenElworthy <98135775+JaydenElworthy@users.noreply.github.com>
2026-06-09 16:18:58 +0530

    Update page.js

--- app/game/page.js
+++ app/game/page.js
@@ -103,7 +103,7 @@ export default function BurgerGame() {
         
         {/* Background Image */}
         <img 
-          src="/images/bbqbackground.jpg" 
+          src="/images/bbqbackground.jpeg" 
           alt="" 
           className="absolute inset-0 w-full h-full object-cover z-0"
         />
