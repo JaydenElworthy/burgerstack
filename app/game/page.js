7fcfad8de78399038b5f59defec2a14ec491c07f
JaydenElworthy <98135775+JaydenElworthy@users.noreply.github.com>
2026-06-10 12:11:37 +0530

    Update page.js

--- app/game/page.js
+++ app/game/page.js
@@ -194,8 +194,9 @@ export default function BurgerGame() {
                 
             <h3 className="w-full text-center text-xs font-medium uppercase text-white opacity-80 tracking-wider mb-4 leading-relaxed block px-2">
   Add your Bun &rarr; Add your Patty &rarr; Add your Cheese &rarr; Add your Bun
-</h3>
-
+</h3> 
+<h2>35 Points To Win A Scratchcard! </h2>
+<br><p> Only 1 Extra Scratchcard to be won a week </p>
             {gameState !== 'start' && (
               <Link href="/" className="w-full flex items-center justify-center gap-2 bg-[#E55937] border-4 border-black text-white py-4 rounded-full font-bold uppercase text-xs shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:bg-[#ff6e4a] transition-colors">
                 <Home size={16} /> Exit to Menu
