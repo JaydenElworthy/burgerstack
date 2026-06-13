704ea7b84d29921a16cdac644159ce53c341849e
JaydenElworthy <98135775+JaydenElworthy@users.noreply.github.com>
2026-06-13 20:25:02 +0100

    Update page.js

--- app/leaderboard/page.js
+++ app/leaderboard/page.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react'
 import { supabase } from '../../lib/supabase'
 import Link from 'next/link'
 import { ArrowLeft, Trophy, Flame, Star } from 'lucide-react'
+import { motion, AnimatePresence } from 'framer-motion';
+
 
 export default function Leaderboard() {
   const [leaders, setLeaders] = useState([])
