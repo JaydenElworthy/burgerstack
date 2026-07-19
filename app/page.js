7c80bf136e30242d6b14710588b8edffb781af41
JaydenElworthy <jayden.harley.elworthy@gmail.com>
2026-07-19 17:29:18 +0530

    Revert PWA standalone redirect to picnicathome.com

--- app/page.js
+++ app/page.js
@@ -107,7 +107,7 @@ export default function Home() {
         return () => {
             window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
         };
-        }, []);
+    }, []);
 
     const handleInstallPWA = async () => {
         if (isIOS) {
